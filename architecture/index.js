888efb4ba76f77d00e1011a4b13ef032bbfec525
Doromido <netsinskajulia@gmail.com>
2024-05-28 19:53:22 +0300

    Added filter to index.js

--- architecture/index.js
+++ architecture/index.js
@@ -18,3 +18,76 @@ function saveToLocalStorage(data) {
 
   document.getElementById('partnerSurvey').addEventListener('submit', handleSubmit);
 
+// Фільтри
+// Отримання результатів фільтрації
+const surveysData = JSON.parse(localStorage.getItem('surveys')) || [];
+const renaissanceLovers = JSON.parse(localStorage.getItem('surveys')).filter(survey => {
+  return survey['favorite-style'] === 'Renaissance';
+});
+console.log(renaissanceLovers);
+
+const notBuyingCourse = JSON.parse(localStorage.getItem('surveys')).filter(survey => {
+  return survey['course'] === 'no';
+});
+console.log(notBuyingCourse);
+
+const gothicFans = JSON.parse(localStorage.getItem('surveys')).filter(survey => {
+  return survey['style'].includes('Gothic');
+});
+console.log(gothicFans);
+
+// Пошук контейнера для відображення результатів
+const resultsContainer2 = document.getElementById('filteredResults');
+
+// Очистка контейнера перед відображенням нових результатів
+resultsContainer2.innerHTML = '';
+
+// Створення та відображення списку результатів для кожного фільтру
+function displayResults(results) {
+  const resultList = document.createElement('ul');
+  results.forEach(result => {
+      const listItem = document.createElement('li');
+      listItem.textContent = `${result.name} - ${result.email}`;
+      resultList.appendChild(listItem);
+  });
+  resultsContainer2.appendChild(resultList);
+}
+
+// Відображення результатів для кожного фільтру
+displayResults(renaissanceLovers);
+displayResults(notBuyingCourse);
+displayResults(gothicFans);
+
+//коміт 2
+//Функція для адаптації навігаційного меню на різних розмірах екранів
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const form = document.querySelector('form');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const formData = new FormData(form);
+        const data = {};
+
+        for (let [key, value] of formData.entries()) {
+            data[key] = value;
+        }
+
+        localStorage.setItem('surveyResults', JSON.stringify(data));
+
+        alert('Результати опитування збережено у LocalStorage.');
+    });
+});
+
+
+
+ 
